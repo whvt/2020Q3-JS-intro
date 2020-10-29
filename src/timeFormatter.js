f90c5c699111f739ed363bcd9ef485a7e45ece24
Nikita <xm987654321@gmail.com>
2020-10-29 16:56:02 +0300

    complete task 1

--- src/timeFormatter.js
+++ src/timeFormatter.js
@@ -1,7 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function timeFornatter(/* sec */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function timeFornatter(sec) {
+  //throw new CustomError('Not implemented');
+  let minutes = Math.floor(sec / 60);
+  let seconds = sec - minutes * 60;
 
+  if (sec < 60) {
+    return `${sec} sec`;
+  } else if (sec % 60 == 0) {
+    return `${sec / 60} min`;
+  } else if (sec > 60 && sec % 60 !== 0) {
+    return `${minutes} min ${seconds} sec`;
+  } else {
+    throw new Error("Error");
+  }
+};
